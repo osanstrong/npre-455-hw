bfbce91c8b7154c6955cc9926cf3c1a886feea28
Owen Strong <owensanstrong@gmail.com>
2026-02-16 16:21:14 -0600

    last edits of hw3

--- hw3util/plot_p8.py
+++ hw3util/plot_p8.py
@@ -7,6 +7,14 @@ PHI_0 = 13.1e13 # cm⁻² s⁻1
 PI = 3.141592653589
 R = 50 # cm
 
+XS_A = 0.107 #cm⁻¹
+XS_F = 0.0727
+NU = 2.4
+
+# Base phi
+def get_phi0(r):
+    return (PHI_0 * R * np.sin(PI*r/R))/r
+
 # Current formula
 def get_Jr(r):
     return -(D * PHI_0 / (r*r)) * (PI*r*np.cos(PI * r / R) - R*np.sin(PI*r/R))
@@ -23,15 +31,25 @@ print(get_crossings(R/2 + 0.5))
 
 
 # Array
-r = np.linspace(1, R, 1000)
+r = np.linspace(0.01, R, 1000)
 
 Jr = get_Jr(r)
+Cr = Jr*4*PI*r*r
+
+
+integ_flux = get_phi0(r)*r*r*4*PI
+absorb = integ_flux*XS_A
+create = integ_flux*XS_F*NU
 
 # Plots
 plt.plot(r, Jr, label="Jr")
+plt.plot(r, Cr, label="Cr")
+plt.plot(r, get_phi0(r), label="phi")
+plt.plot(r, absorb, label="Neutrons absorbed in unit layer")
+plt.plot(r, create, label="Neutrons generated in unit layer")
 
 plt.xlabel("r (cm)")
-plt.ylabel("Current Jr (cm⁻²s⁻¹)")
+plt.ylabel("Current Jr (cm⁻²s⁻¹) and crossings Cr (s⁻¹)")
 
 plt.legend()
 plt.show()
